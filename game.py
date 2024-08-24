7fd1304b8ab281f8f059b57917dcde8a994dcafd
Bagath <132096909+Bagathexe@users.noreply.github.com>
2024-08-24 23:35:00 +0530

    New window

---
+++ game.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import random
+
+def next_Turn():
+    pass
+
+def check_winner():
+    pass
+
+def empty_spaces():
+    pass
+
+def new_game():
+    pass
+
+window = Tk()
+
+window.mainloop()
+

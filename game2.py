aa2f1af903c2686c17463a1e9eece73558579739
Bagath <132096909+Bagathexe@users.noreply.github.com>
2024-08-25 20:30:24 +0530

    Update game2.py

--- game2.py
+++ game2.py
@@ -85,7 +85,7 @@ label = Label(text=player + " turn", font=('consolas', 40))
 label.pack(side="top")
 
 reset_button = Button(text="Restart", font=('consolas', 20), command=new_game)
-reset_button.pack(side="top")
+reset_button.pack(side="bottom")
 
 frame = Frame(window)
 frame.pack()
